4c50aedeb5002b592e1f72950f9a8ae7e3d1fdc2
Ana Raquel Matos <anaraquelpmatos@gmail.com>
2022-03-14 15:52:10 -0300

    Adição do Fundo Mobile

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom";
 import Navbar from "./Navbar";
 import Corpo from "./Corpo";
+import FundoMobile from "./FundoMobile";
 
 function Instagram() {
     return (
         <div>
         <Navbar />
         <Corpo />
+        <FundoMobile />
         </div>
     );
 }
